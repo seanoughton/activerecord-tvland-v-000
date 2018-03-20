17822fea502e4af80200b65e591bb72dcc11254d
Sean Oughton <seanoughton@gmail.com>
2018-03-20 00:40:26 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,6 +6,9 @@ class Show < ActiveRecord::Base
 
   def build_network(hash)
     #Character can chain-build associations to which it belongs
+    # to do this, the show model has to define its relationship with network
+    #show.build_network(:call_letters => "NBC")
+    #expect(show.network.call_letters).to eq("NBC")
   end
 
 
