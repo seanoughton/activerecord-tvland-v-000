ecb19d3280eb8597ee873f4418c9509566dc9335
Sean Oughton <seanoughton@gmail.com>
2018-03-20 00:19:51 +0000

    automatically backed up by learn

--- db/migrate/007_add_genre_to_shows.rb
+++ db/migrate/007_add_genre_to_shows.rb
@@ -1,4 +1,4 @@
-class AddNameGenreToShows < ActiveRecord::Migration[4.2]
+class AddGenreToShows < ActiveRecord::Migration[4.2]
   def change
     #add_column :tablename, :columnname, :type
     add_column :shows, :genre, :string
