e7f4f31c2f87fbbac1c371dc4242e25d35178368
Sean Oughton <seanoughton@gmail.com>
2018-03-20 00:27:40 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
 
+  def build_network(something)
+    #Character can chain-build associations to which it belongs
+  end
 
 
 
